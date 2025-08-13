3c17d80f46b070b32ad1471c5d91b16f8105e329
twiceM <mohamed98mahmoud2016@gmail.com>
2025-08-13 21:08:52 +0300

    feat: Set up basic NestJS application structure
    Create core application components including app module, controller, and service.

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { LoggingModule } from './logging/logging.module';
+
+const mongooseImport =
+  process.env.NODE_ENV === 'test'
+    ? []
+    : [
+        MongooseModule.forRootAsync({
+          inject: [ConfigService],
+          useFactory: (config: ConfigService) => {
+            const uri = config.get<string>('MONGO_URI');
+            if (!uri) {
+              throw new Error('MONGO_URI is not defined');
+            }
+            return { uri };
+          },
+        }),
+      ];
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+    }),
+    ...mongooseImport,
+    LoggingModule,
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('{api}');
+
+  //Initialize Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('Authentication API (NestJS + Mongo)')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  //Initialize Logger
+  app.useLogger(app.get(Logger));
+
+  //Start server
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
